9027e0fe264d5fe6818ff09281065f1b3b56e324
tsahaca <9325425+tsahaca@users.noreply.github.com>
2023-06-17 08:16:36 -0700

    Create README.m

---
+++ DevOps/README.m
@@ -0,0 +1,20 @@
+
+# DevOps RoadMap
+
+Learning DevOps involves acquiring knowledge and skills in various areas related to software development, operations, automation, and collaboration.
+
+
+Here's a roadmap to guide your learning journey in DevOps
+
+  1. Learn at least one programming language like Python, Go, or JavaScript to write automation scripts
+  2. Learn server management, including working with proxies, caching servers, and web servers like Nginx, Apache, or IIS.
+  3. Gain knowledge of containerization using Docker, including running containers, Docker networking, volumes, and Dockerfiles.
+  4. Learn container orchestration using Kubernetes for automated deployment, scaling, and networking of containers.
+  5. Explore Infrastructure as Code (IaC) using tools like Terraform, Ansible, Chef, or Puppet for provisioning and configuration management.
+  6. Please familiarise yourself with Linux and its command-line interface (CLI), with Ubuntu being a recommended distribution.
+  7. Understand network protocols, including DNS, IP addresses, ports, and the OSI model.
+  8. Understand Continuous Integration/Continuous Deployment (CI/CD) practices for automating app delivery and deployment stages.
+  9. Familiarise yourself with monitoring techniques for real-time applications, services, and infrastructure oversight.
+ 10. Gain experience with cloud providers like AWS and Azure, leveraging their APIs for abstracting and provisioning infrastructure.
+
+Remember, DevOps is a mindset and a set of continually evolving practices. As you progress through the roadmap, adapt and refine your learning path based on your interests, goals, and the specific requirements of your organisation or projects. Hands-on experience and practical application of concepts are essential for mastering DevOps.
