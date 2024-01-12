23ecd094e29006d4ac11759b24069d8b5a8416d1
tsahaca <9325425+tsahaca@users.noreply.github.com>
2024-01-12 10:28:43 -0800

    Update SPARK_JOBDESCRIPTION.m

--- AWS/EMR/SPARK_JOBDESCRIPTION.m
+++ AWS/EMR/SPARK_JOBDESCRIPTION.m
@@ -1,3 +1,2 @@
-https://www.gresearch.com/blog/article/enhancing-sparks-ui-with-job-descriptions/
-
-https://aws.amazon.com/blogs/devops/a-new-spark-plugin-for-cpu-and-memory-profiling/
+. https://www.gresearch.com/blog/article/enhancing-sparks-ui-with-job-descriptions/
+. https://aws.amazon.com/blogs/devops/a-new-spark-plugin-for-cpu-and-memory-profiling/
